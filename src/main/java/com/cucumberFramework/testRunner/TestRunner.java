5f8b73e57adf981f2f58852c53247ce75cd2585c
aniketfar <61340984+aniketfar@users.noreply.github.com>
2021-09-11 01:31:41 +0530

    Update TestRunner.java

--- src/main/java/com/cucumberFramework/testRunner/TestRunner.java
+++ src/main/java/com/cucumberFramework/testRunner/TestRunner.java
@@ -15,7 +15,7 @@ import cucumber.api.testng.TestNGCucumberRunner;
 				plugin = { "pretty", "html:target/cucumber-reports/cucumber-pretty",
 						"json:target/cucumber-reports/CucumberTestReport.json", 
 						"rerun:target/cucumber-reports/rerun.txt" },
-				monochrome = false,
+				monochrome = true,
 				dryRun = false)
 //tags = {}
 
