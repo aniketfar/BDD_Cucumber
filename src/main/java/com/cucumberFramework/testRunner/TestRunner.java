b0feeac63fd3a77640e7558ba96f189da358b3ad
aniketfar <61340984+aniketfar@users.noreply.github.com>
2021-09-10 23:20:57 +0530

    added on line

--- src/main/java/com/cucumberFramework/testRunner/TestRunner.java
+++ src/main/java/com/cucumberFramework/testRunner/TestRunner.java
@@ -17,7 +17,7 @@ import cucumber.api.testng.TestNGCucumberRunner;
 						"rerun:target/cucumber-reports/rerun.txt" },
 				monochrome = true, 
 				dryRun = false)
-//tags = {};
+//tags = {};//tags = {};
 
 public class TestRunner {
 	
